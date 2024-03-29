6a2b95f2859af6c5613a12873996f6d9d17cd71c
FraySantiago <santiagofontalvo13@gmail.com>
2024-03-29 14:01:31 -0500

    terminado modulo registro usuario

--- Services/register_user.js
+++ Services/register_user.js
@@ -3,32 +3,32 @@ import { registerauth } from "../Services/firebase.js"
 const save_auth = document.getElementById('btn_register')
 
 async function register() {
-    const nombreInput = document.getElementById('nombre').value
-    const apellidoInput = document.getElementById('apellido').value
-    const emailInput = document.getElementById('emailR').value
-    const contraseñaInput = document.getElementById('contraseñaR').value
+    if (window.verificado === true) {
+        const emailInput = document.getElementById('emailR').value
+        const contraseñaInput = document.getElementById('contraseñaR').value
 
-    if (nombreInput.trim() === '' || apellidoInput.trim() === '' || emailInput.trim() === '' || contraseñaInput.trim() === '') {
-        alert('Por favor, complete todos los campos.');
-        return;
+        const validar = registerauth(emailInput, contraseñaInput)
+        const verificar = await validar
+
+            .then((verificar) => {
+                alert("El usuario se registro exitosamente..")
+                const user = verificar.user;
+                window.location.href = "../index.html"
+            })
+
+            .catch((error) => {
+                if (error.code === 'auth/email-already-in-use') {
+                    alert("Este correo electrónico ya está en uso. Por favor, utiliza otro.")
+                } else if(error.code === 'auth/invalid-email') {
+                    alert("Correo invalido, sigue el formato: ejemplo@example.com")
+                }else{
+                    alert("Hubo un error, intenta de nuevo más tarde.")
+                    console.error(error)
+                }
+            })
     }
-    
-    const validar = registerauth(emailInput, contraseñaInput)
-    const verificar = await validar
-
-    .then((verificar) => {
-        alert("El usuario se registro exitosamente..")
-        const user = verificar.user;
-        window.location.href = "../index.html"
-
-    })
-
-    .catch((error) => {
-        alert("Hubo un error, intenta mas tarde")
-        const errorCode = error.code;
-        const errorMessage = error.message;
-    })
 }
+
 
 window.addEventListener('DOMContentLoaded', () => {
     save_auth.addEventListener('click', register);
