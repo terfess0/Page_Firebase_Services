e8f181232f1311f712300214eac3e8c1e919aea4
FraySantiago <santiagofontalvo13@gmail.com>
2024-04-22 14:13:20 -0500

    .

--- Services/auth_facebook.js
+++ Services/auth_facebook.js
@@ -1,36 +1,36 @@
-import { popup_facebook } from "../Services/firebase.js";
+import { popup_facebook } from "../Services/firebase.js"
 
-const btnlogfacebook = document.getElementById("btn_log_facebook");
+const btnlogfacebook = document.getElementById("btn_log_facebook")
 
 async function log_facebook() {
     try {
-        const verification = await popup_facebook();
+        const verification = await popup_facebook()
         if (verification != null) {
-            alert("Usuario autenticado: " + verification.email);
-            window.location.href = "Templates/home.html";
+            alert("Usuario autenticado: " + verification.email)
+            window.location.href = "Templates/home.html"
         } else {
-            console.log("Sesión no validada");
-            alert("Error de usuario, verifique usuario y/o contraseña.");
+            console.log("Sesión no validada")
+            alert("Error de usuario, verifique usuario y/o contraseña.")
         }
     } catch (error) {
-        console.error("Error al autenticar:", error);
+        console.error("Error al autenticar:", error)
         if (error.code === "auth/popup-closed-by-user") {
             // El usuario cerró la ventana de inicio de sesión
-            alert("Iniciar sesión con Facebook cancelado.");
+            alert("Iniciar sesión con Facebook cancelado.")
         } else if (error.code === "auth/account-exists-with-different-credential") {
             // El ya esta registrado con otro metodo
-            alert("Usuario ya esta registrado con otro metodo (Correo o Google), Por favor inicie sesión.");
-            window.location.href = "../index.html";
+            alert("Usuario ya esta registrado con otro metodo (Correo o Google), Por favor inicie sesión.")
+            window.location.href = "../index.html"
         } else {
-            alert("Error de autenticación. Verifica la consola para más detalles.");
+            alert("Error de autenticación. Prueba mas tarde.")
         }
     }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     btnlogfacebook.addEventListener('click', async (e) => {
-        console.log("login facebook");
-        e.preventDefault(); 
-        await log_facebook();
-    });
-});
+        console.log("login facebook")
+        e.preventDefault()
+        await log_facebook()
+    })
+}) 
--- Services/regist_facebook.js
+++ Services/regist_facebook.js
@@ -1,36 +1,36 @@
-import { popup_facebook } from "../Services/firebase.js";
+import { popup_facebook } from "../Services/firebase.js" 
 
-const btnregistfacebook = document.getElementById("btn_regist_facebook");
+const btnregistfacebook = document.getElementById("btn_regist_facebook") 
 
 async function regist_facebook() {
     try {
-        const verification = await popup_facebook();
+        const verification = await popup_facebook() 
         if (verification != null) {
-            alert("Usuario autenticado: " + verification.email);
-            window.location.href = "../Templates/home.html";
+            alert("Usuario autenticado: " + verification.email) 
+            window.location.href = "../Templates/home.html" 
         } else {
-            console.log("Sesión no validada");
-            alert("Error de usuario, verifique usuario y/o contraseña.");
+            console.log("Sesión no validada") 
+            alert("Error de usuario, verifique usuario y/o contraseña.") 
         }
     } catch (error) {
-        console.error("Error al autenticar:", error);
+        console.error("Error al autenticar:", error) 
         if (error.code === "auth/popup-closed-by-user") {
             // El usuario cerró la ventana de inicio de sesión
-            alert("Iniciar sesión con Facebook cancelado.");
+            alert("Iniciar sesión con Facebook cancelado.") 
         } else if (error.code === "auth/account-exists-with-different-credential") {
             // El ya esta registrado con otro metodo
-            alert("Usuario ya esta registrado con otro metodo (Correo o Google), Por favor inicie sesión.");
-            window.location.href = "../index.html";
+            alert("Usuario ya esta registrado con otro metodo (Correo o Google), Por favor inicie sesión.") 
+            window.location.href = "../index.html" 
         } else {
-            alert("Error de autenticación. Verifica la consola para más detalles.");
+            alert("Error de autenticación. Prueba mas tarde.")
         }
     }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     btnregistfacebook.addEventListener('click', async (e) => {
-        console.log("registro facebook");
-        e.preventDefault();
-        await regist_facebook();
-    });
-});
+        console.log("registro facebook") 
+        e.preventDefault() 
+        await regist_facebook() 
+    }) 
+}) 
