1bdd84f612f65deff11cae49762e8b33efb2fd09
Terfess <73000354+terfess0@users.noreply.github.com>
2024-05-09 07:35:29 -0500

    x

--- Services/get_products.js
+++ Services/get_products.js
@@ -1,21 +1,27 @@
 import { getDataProducts } from "../Services/firebase.js";
 
+const ver = document.getElementById("vdata");
 
-const ver = document.getElementById("vdata")
+async function cargar() {
+  ver.innerHTML = "";
+  const docref = getDataProducts();
 
-async function cargar(){
-    ver.innerHTML = ""
-    const docref = getDataProducts()
-    
-    const querySnapshot = await docref
+  const querySnapshot = await docref;
 
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
 
-        //ver.innerHTML += ``
-      });
+    ver.innerHTML += `
+        <tr>
+        <th scope = "row">${doc.data().codigo})</th>
+        <td>${doc.data().nombre}</td>
+        <td>${doc.data().descripcion}</td>
+        <td>${doc.data().cantidad}</td>
+        </tr>
+        `;
+  });
 }
 
-window.addEventListener('DOMContentLoaded', async() =>{
-    cargar()
-})
+window.addEventListener("DOMContentLoaded", async () => {
+  cargar();
+});
