d8c9e741a1f1e92ee26c1628b492e12049b15450
terfess0 <santiagofontalvo13@gmail.com>
2024-05-16 11:32:42 -0500

    change: direccion pagina de retorno, ahora va a home_admin add: opcion de elimimar los usuarios de la tabla

--- Services/view_users.js
+++ Services/view_users.js
@@ -1,28 +1,60 @@
-import { getDataUsers} from "../Services/firebase.js"
+import { getDataUsers, getDocUser, deleteDataUser } from "../Services/firebase.js";
 
-const contain = document.getElementById("usersData")
+const contain = document.getElementById("usersData");
 
-async function view_users(){
-
+async function view_users() {
     contain.innerHTML = "";
     const users = getDataUsers();
-  
     const query = await users;
-  
-    query.forEach((doc) => {
-  
-        contain.innerHTML += `
-          <tr>
-          <th scope = "row">${doc.data().userIdentification}</th>
-          <td>${doc.data().userName}</td>
-          <td>${doc.data().userEmail}</td>
-          <td>${doc.data().userBirthDate}</td>
-          </tr>
-          `;
-    });
 
+    query.forEach((doc) => {
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <th scope="row">${doc.data().userIdentification}</th>
+            <td>${doc.data().userName}</td>
+            <td>${doc.data().userEmail}</td>
+            <td>${doc.data().userBirthDate}</td>
+            <td><button class="btn btn-danger">Borrar</button></td>
+        `;
+
+        // Añadir evento de clic para el botón de borrar
+        row.querySelector('button').addEventListener('click', () => delete_data(doc.id));
+
+        contain.appendChild(row);
+    });
 }
 
-window.addEventListener("DOMContentLoaded", async => {
-    view_users()
-})
+
+async function delete_data(docUserId) {
+    console.log("Tratando de borrar");
+
+    try {
+        const action = deleteDataUser(docUserId)
+        const borrar = await action
+        alert("Usuario eliminado.");
+        //recargar la página
+        location.reload();
+
+
+    } catch (error) {
+        console.error("Error al eliminar datos de usuario:", error.code);
+
+        switch (error.code) {
+            case 'permission-denied':
+                alert("Permisos insuficientes para eliminar el usuario.");
+                break;
+            case 'not-found':
+                alert("Usuario no encontrado.");
+                break;
+            default:
+                alert("Error al eliminar usuario: " + error.message);
+                break;
+        }
+    }
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    view_users();
+});
